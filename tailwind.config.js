d76e0971e67bfe3860f2c28c570b5f1083eed7e0
Tanishkpandey <pandeytanishk@gmail.com>
2024-01-08 10:29:09 +0530

    Updated for large Screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ["./dist/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkblue: '#00050f',
+        midblue: "#71BEE5",
+        offwhite: "#c2c2c2",
+        white: "#ffffff"
+      },
+      fontFamily: {
+        'jakarta': ['Plus Jakarta Sans', 'sans-serif'],
+        'inter': ['Inter', 'sans-serif']
+      },
+      borderWidth: {
+        DEFAULT: '1px',
+        '0': '0',
+        '2': '2px',
+        '3': '3px',
+        '4': '4px',
+        '6': '6px',
+        '8': '8px',
+      }
+    },
   },
   plugins: [],
 }
